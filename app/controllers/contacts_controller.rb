38a5867fc6f939fc2633d542ad3387a047fbf07f
Nick Goddard <nicko777@gmail.com>
2011-05-30 13:06:09 +0100

    fixing contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
 	def new
-		@contact = Contact.new(:id => 1)
+		@contact = Contact.new
 	end
 
 	def create
@@ -13,3 +13,4 @@ class ContactsController < ApplicationController
 		end
 	end
 end
+
